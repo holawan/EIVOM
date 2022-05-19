c4f3bf1a5ed5bd930bfd8b8459cbf98adf971a6e
holawan <asdf134652@gmail.com>
2022-05-19 21:05:25 +0900

    [FEAT] app생성, settings 설정 accounts 앱 생성 및 migration crews,movies 앱 생성 auth 관련 settings 설정

--- final-pjt-back/apiserver/settings.py
+++ final-pjt-back/apiserver/settings.py
@@ -40,15 +40,39 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #app
+    'accounts',
+    'movies',
+    'crews',
+
+    #3rd-party
+    'django_extensions',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    #DRF auth
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    #signup
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    #cors
+    'corsheaders',
+    #local
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',  # dj-rest-auth signup 필요
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -112,9 +136,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -132,3 +156,29 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+# 특정 origin 에게만 교차 출처 허용
+# CORS_ALLOWED_ORIGINS = [
+#     # Vue LocalHost
+#     'http://localhost:8080',
+#     'http://127.0.0.1:8001',
+# ]
+
+# 모두에게 교차출처 허용 (*)
+CORS_ALLOW_ALL_ORIGINS = True
+
+# DRF 인증 관련 설정
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 모두에게 허용
+        # 'rest_framework.permissions.AllowAny', 
+
+        # 인증된 사용자만 모든일이 가능 / 비인증 사용자는 모두 401 Unauthorized
+        'rest_framework.permissions.IsAuthenticated'
+    ]
+}

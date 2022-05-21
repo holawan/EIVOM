c54d092e51f9f37a5664710f6564e4c3f684431a
holawan <asdf134652@gmail.com>
2022-05-21 21:33:41 +0900

    [FIX] image upload error

--- final-pjt-front/src/views/accounts/CreateProfile.vue
+++ final-pjt-front/src/views/accounts/CreateProfile.vue
@@ -3,8 +3,11 @@
     <h1>프로필 초기 설정</h1>
     <div>
       
-      <form @submit.prevent="signup(credentials)">
-        <input multiple @change="onInputImage()" rec="image" type="file">
+      <form @submit.prevent="createProfile(credentials)" enctype="multipart/form-data">
+        <label for="image">프로필사진 입력: </label>
+        <input multiple @change="onInputImage()" ref="image" type="file" id="image">
+        <label for="backdrop">배경사진 입력: </label>
+        <input multiple @change="onInputImage2()" ref="backdrop" type="file" id="backdrop">
         <div>
           <label for="nickname">닉네임 입력: </label>
           <input  v-model="credentials.nickname" type="text" id="nickname" required/>
@@ -14,9 +17,21 @@
           <input v-model="credentials.birth" type="date" id="birth" required />
         </div>
         <div>
-          <label for="introduction">프로필소개:</label>
-          <input v-model="credentials.introduction" type="text" id="introduction" required />
+          <label for="introduce">프로필소개:</label>
+          <input v-model="credentials.introduce" type="text" id="introduce" required />
         </div>
+          <div>
+            <label for="gender">성별:</label>
+            <input v-model="credentials.gender" type="text" id="gender" required />
+          </div>
+          <div>
+            <label for="location1">시/도:</label>
+            <input v-model="credentials.location1" type="text" id="location1" required />
+          </div>
+          <div>
+            <label for="location2">시/군/구:</label>
+            <input v-model="credentials.location2" type="text" id="location2" required />
+          </div>
         <div>
           <button>Signup</button>
         </div>
@@ -32,14 +47,27 @@ export default {
   data(){
     return {
       credentials: {
+        image :'',
+        backdrop :'',
         nickname: '',
         birth: '',
-        introduction: '',
+        introduce: '',
+        gender: '',
+        location1: '',
+        location2: '',
       }
     }
   },
   methods: {
-    ...mapActions(['createProfile'])
+    ...mapActions(['createProfile']),
+    onInputImage() {
+      this.image = this.$refs.image.files
+
+    },
+    onInputImage2() {
+      this.backdrop = this.$refs.backdrop.files
+
+    }
   }
 
 }

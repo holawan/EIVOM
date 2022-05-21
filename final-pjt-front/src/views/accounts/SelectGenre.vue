b494a1b2173794190619aae4afe77d002a4f26b6
unhhyyeexx <unhhyyee66@gmail.com>
2022-05-22 01:29:20 +0900

    [ERROR] selectgenre

--- final-pjt-front/src/views/accounts/SelectGenre.vue
+++ final-pjt-front/src/views/accounts/SelectGenre.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
     <h1>Select Genre</h1>
+    {{genres}}
+    <p v-for="genre in genres" :key="genre.id">
+      {{genre.name}}
+    </p>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters, mapState } from 'vuex'
 export default {
-  name: 'SelectGenre'
+  name: 'SelectGenre',
+  // data(){
+
+  // },
+  computed:{
+    ...mapGetters(['genres']),
+    ...mapState(['genres'])
+  },
+  methods:{
+    ...mapActions(['loadGenre']),
+    
+  },
+  created(){
+    this.loadGenre()
+
+  }
 
 }
 </script>

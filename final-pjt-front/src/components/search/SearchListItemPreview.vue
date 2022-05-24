fee22e89aeb0472502ffcfe150cc7c7b646ccff5
holawan <asdf134652@gmail.com>
2022-05-25 01:37:21 +0900

    [ERROR] search bar

---
+++ final-pjt-front/src/components/search/SearchListItemPreview.vue
@@ -0,0 +1,30 @@
+<template>
+  <li >
+    {{ movie.title }}
+  </li>
+  
+</template>
+
+<script>
+export default {
+  name: 'SearchListItem',
+  props: {
+    movie: {}
+  },
+  methods: {
+    onClickItem () {
+      this.$emit('on-click-item', this.movie)
+    }
+  },
+}
+</script>
+
+<style>
+  .item {
+    cursor: pointer;
+  }
+
+  .item:hover {
+    background: #eee;
+  }
+</style>

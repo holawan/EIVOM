9a8da710f3c6e543432586f1b21e57dfa4925f29
unhhyyeexx <unhhyyee66@gmail.com>
2022-05-26 04:14:28 +0900

    [STYLE] select genre css

--- final-pjt-front/src/views/accounts/SelectGenre.vue
+++ final-pjt-front/src/views/accounts/SelectGenre.vue
@@ -1,38 +1,42 @@
 <template>
   <div>
-      <div class="gerneBackground">
-        <br>
-        <h1>선호하는 장르를 골라주세요</h1>
-        <h3>회원님을 위한 추천영화 데이터로 사용됩니다.</h3>
-        <br>
-        <div class="container">
+    <div class="gerneBackground">
+      <br>
+      <h1 class="header">선호하는 장르를 골라주세요!</h1>
+      <h4 class="header">회원님을 위한 추천영화 데이터로 사용됩니다.</h4>
+      <br>
+      <div class="container">
 
-          <div class="row">
+        <div class="row">
 
-            <div v-for="genre in genres" :key="genre.pk" class="col-3 d-flex justify-content-center align-items-center my-3">
-              <button
-              class="btn btn-secondary  btn-lg rounded-pill text-black"
-              style="background-color: white ; border-width: 6px; width: 150px; height: 60px;"
-              :style="{borderColor : colors}"
-                @click="select(genre.pk)"
-              >{{ genre.name }}</button><br>
-            </div>
+          <div v-for="genre in genres" :key="genre.pk" class="col-3 d-flex justify-content-center align-items-center my-3">
+            <button
+            class="btn btn-secondary  btn-lg rounded-pill text-black"
+            style="background-color: white; border-style:none; font-weight:600; width: 150px; height: 60px; box-shadow: 2px 2px 2px green;"
+              @click="select(genre.pk)"
+            >{{ genre.name }}</button><br>
           </div>
         </div>
+      </div>
+
+      <!-- submit button -->
+      <div>
         <div v-if="selectedGenre.length >= 3">
           <router-link :to="{name: 'Main'}">
-            <button class="btn btn-primary">
+            <button class="btn btn-light btn-lg rounded-pill my-3" style="font-weight:600; width: 150px; height: 60px; box-shadow: 2px 2px 2px gray;">
               제출
             </button>
           </router-link>
         </div>
         <div v-else>
-          <button
+          <button class="btn btn-light btn-lg rounded-pill my-3" style="font-weight:600; width: 150px; height: 60px; box-shadow: 2px 2px 2px gray;"
             @click="goalert()"
             disabled="disabled"
           >제출</button>
         </div>
+      </div>
       <br>
+
       <!-- 선택한 장르 ui -->
       <div class="d-flex justify-content-center">
 
@@ -42,7 +46,10 @@
           :genre="genre"
           
         >
-          <button class=" btn btn-secondary rounded-pill mx-2" @click="select(genre.pk)" >{{ genre.name }}</button>
+          <button 
+            class=" btn btn-secondary rounded-pill mx-2" 
+            @click="select(genre.pk)" 
+            style="width:100px; height:40px" >{{ genre.name }}</button>
         </div>
       </div>
     </div>
@@ -57,7 +64,6 @@ export default {
   data(){
     return{
       selectedGenre : [],
-      colors : `hsl(${parseInt(Math.random() * 24, 10) * 15}, 16%, 57%)`
     }
   },
   computed:{
@@ -103,7 +109,14 @@ export default {
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  opacity: 90%;
   /* position: sticky; */
 }
 
+.header{
+  color: aliceblue;
+  font-weight: 500;
+  text-shadow: 2px 2px 2px gray;
+
+}
 </style>

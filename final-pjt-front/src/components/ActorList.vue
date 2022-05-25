2e83940258201eb9c4b43f288f9676d5ef719339
holawan <asdf134652@gmail.com>
2022-05-26 04:16:56 +0900

    [FIX] movie detail view

--- final-pjt-front/src/components/ActorList.vue
+++ final-pjt-front/src/components/ActorList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
 
-    <div
+    <!-- <div
       v-for="actor,idx in this.movie.actors"
       :key="idx"
       :idx="idx"
@@ -14,7 +14,26 @@
         </div>
       </router-link>
 
+    </div> -->
+
+    <div class="row">
+      <div class="d-flex justify-content-center col-2"
+        v-for="actor,idx in this.movie.actors"
+        :key="idx"
+        :idx="idx"
+      >
+        <router-link :to="{name: 'CastDetail', params: { castId:movie.actor_id[idx] } }" style="text-decoration: none; color: black; font-weight: bold; ">
+          <div v-if="idx<6" class=" actor_card mx-auto" style="width: 16rem;  border:3px solid black; border-radius: 1em;">
+            <img class="card-img-top mb-2 mx-0 " :src="'https://image.tmdb.org/t/p/w400/'+ movie.actors_path[idx]" alt="Card image cap" style="width:100%; border-top-left-radius: 0.7rem;border-top-right-radius: 0.7rem; height:20rem; box-sizing:content-box;">
+            <br>
+              <h5 class="mt-1" style="text-align: center; ">{{ movie.actors[idx] }}</h5>
+          </div>
+        </router-link>
+
     </div>
+
+    </div>
+        <div class="swiper-pagination" slot="pagination"></div>
   </div>
 </template>
 
@@ -28,19 +47,18 @@ export default {
     props:{ 
     movieId:null ,
   },
-  // data() {
-  //   return {
-  //     nowLen : this.movie.length,
-  //   }
-  // },
-  computed: {
+  components: {
+    
+  },
+   computed : {
     ...mapGetters(['movie']),
   },
-   methods: {
-    ...mapActions([
-      'fetchMovie',
-    ])
+  methods : {
+
+        ...mapActions(['fetchMovie' ])
   },
+
+
     created(){
     if(this.movie.length) {
       console.log('있어!')

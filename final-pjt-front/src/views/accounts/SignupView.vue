018fa89758cb1b80a8601307e135694d04e04234
unhhyyeexx <unhhyyee66@gmail.com>
2022-05-25 15:48:07 +0900

    [FIX] weather error fix

--- final-pjt-front/src/views/accounts/SignupView.vue
+++ final-pjt-front/src/views/accounts/SignupView.vue
@@ -1,27 +1,62 @@
 <template>
-  <div>
+  <div id="content">
     <h1>회원가입</h1>
 
     <account-error-list v-if="authError"></account-error-list>
 
     <form @submit.prevent="signup(credentials)">
+      
+      <!-- email -->
       <div>
-        <label for="email">아이디: </label>
-        <input v-model="credentials.email" type="email" id="email"  required/>
-        <!-- <span class="badge badge-danger mt-1" v-if="!availableEmail">이미 사용중인 이메일입니다.</span>
-        <span class="badge badge-danger mt-1" v-if="!availableEmailForm">이메일 형식이 다릅니다.</span> -->
+        <h3>
+          <label for="email"></label>
+        </h3>
+        <span class="box int_id">
+          <input class="id" v-model="credentials.email" type="email" id="email" placeholder="아이디 입력" ng-required="true"/>
+        </span>
       </div>
+
+      <!-- password1 -->
       <div>
-        <label for="password1">비밀번호: </label>
-        <input v-model="credentials.password1" type="password" id="password1" required/>
+        <h3 class="join_title"><label for="password1">비밀번호</label></h3>
+        <span class="box int_pass">
+            <input class="int" v-model="credentials.password1" type="password" id="password1" placeholder="비밀번호 입력" ng-required="true"/>
+            <span id="alertTxt">사용불가</span>
+        </span>
+        <span class="error_next_box"></span>
       </div>
+
+      <!-- password2 -->
       <div>
-        <label for="password2">비밀번호 확인: </label>
-        <input v-model="credentials.password2" type="password" id="password2" required/>
+        <h3 class="join_title"><label for="password2">비밀번호 확인</label></h3>
+        <span class="box int_pass_check">
+            <input class="int" v-model="credentials.password2" type="password" id="password2" placeholder="비밀번호 입력" ng-required="true"/>
+            <span id="alertTxt">사용불가</span>
+        </span>
+        <span class="error_next_box"></span>
       </div>
-      <div>
-        <button>회원가입</button>
+
+      <!-- join button -->
+      <div class="btn_area">
+        <button type="submit" id="btnJoin">
+            <span>회원가입</span>
+        </button>
       </div>
+
+        <!-- <label for="email">
+          <input class="input" v-model="credentials.email" type="email" id="email" placeholder="아이디 입력" ng-required="true"/>
+        </label>
+
+        <b-form-group label for="password1">
+          <input class="input" v-model="credentials.password1" type="password" id="password1" placeholder="비밀번호 입력" ng-required="true"/>
+        </b-form-group>
+
+        <b-form-group label for="password2">
+          <b-form-input class="input" v-model="credentials.password2" type="password" id="password2" placeholder="비밀번호 확인" ng-required="true"/>
+        </b-form-group>
+
+        <b-button size="lg" variant="success" type="submit">Signup</b-button> -->
+
     </form>
   </div>
 </template>
@@ -49,31 +84,97 @@ export default {
   },
   methods: {
     ...mapActions(['signup']),
-    // async checkDuplicate() {
-    //   //일단은 사용가능한 이메일로 true로 초기화 한다.
-    //   this.availableEmail = true;
-        
-    //     //이메일 유효성을 검사한다.
-    //   if (!validateEmail(this.email)) {
-    //     //유효성이 틀리다면 data 값을 false로 한다.
-    //         this.availableEmailForm = false;
-    //     return;
-    //   } else {
-    //     this.availableEmailForm = true;
-    //   }
-        
-    //     //이메일 중복체크를 한다.
-    //   const response = await checkDuplicateEmail(this.email);
-    //   if (!response.data) {
-    //     this.availableEmail = false;
-    //   } else {
-    //     this.availableEmail = true;
-    //   }
   },
 }
 
 </script>
 
 <style>
+#content {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+    width: 460px;
+}
+
+input:focus {
+    outline: none;
+}
+
+h3 {
+    margin: 19px 0 8px;
+    font-size: 14px;
+    font-weight: 700;
+}
+
+.box {
+    display: block;
+    width: 100%;
+    height: 51px;
+    border: solid 1px #dadada;
+    border-radius: 10px;
+    padding: 10px 14px 10px 14px;
+    box-sizing: border-box;
+    background: #fff;
+    position: relative;
+}
+
+.int {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 29px;
+    border: none;
+    background: #fff;
+    font-size: 15px;
+}
+
+input {
+    font-family: Dotum,'돋움',Helvetica,sans-serif;    
+}
+
+.box.int_id {
+    padding-right: 110px;
+}
+
+.box.int_pass {
+    padding-right: 40px;
+}
+
+.box.int_pass_check {
+    padding-right: 40px;
+}
+
+.error_next_box {
+    margin-top: 9px;
+    font-size: 12px;
+    color: red;    
+    display: none;
+}
+
+#alertTxt {
+    position: absolute;
+    top: 19px;
+    right: 38px;
+    font-size: 12px;
+    color: red;
+    display: none;
+}
+
+.btn_area {
+    margin: 30px 0 91px;
+}
+
+#btnJoin {
+    width: 100%;
+    padding: 21px 0 17px;
+    border: 0;
+    cursor: pointer;
+    color: #fff;
+    background-color: #fdfdb1;
+    font-size: 20px;
+    font-weight: 400;
+    font-family: Dotum,'돋움',Helvetica,sans-serif;
+}
 
 </style>

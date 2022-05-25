1732437b4ae5b174a02f5723e3e3b508a74f454c
holawan <asdf134652@gmail.com>
2022-05-25 22:21:31 +0900

    [fix] movielist

--- final-pjt-front/src/components/MovieListItem2.vue
+++ final-pjt-front/src/components/MovieListItem2.vue
@@ -2,16 +2,16 @@
   <div class="item">
     <div v-if="movie.pk">
       <router-link :to="{name: 'MovieDetail', params: { movie_pk : movie.pk} }">
-        <div class="card  pr-3 embed-responsive embed-responsive-1by1" >
-          <img :src="'https://image.tmdb.org/t/p/w400/'+ movie.poster_path" class="card-img-top embed-responsive-item" alt="...">
+        <div class="card  pr-3 embed-responsive embed-responsive-1by1" style="width: 18rem;padding-left:0px;" >
+          <img :src="'https://image.tmdb.org/t/p/w400/'+ movie.poster_path" class="card-img-top embed-responsive-item" alt="..." style="width:18rem; height:25rem;">
         </div>
       </router-link>
     </div>
 
     <div v-if="movie.id">
       <router-link :to="{name: 'MovieDetail', params: { movie_pk : movie.id} }">
-        <div class="card pr-3">
-          <img :src="'https://image.tmdb.org/t/p/w400/'+ movie.poster_path" class="card-img-top" alt="...">
+        <div class="card pr-3" style="width: 18rem;padding-left:0px;">
+          <img :src="'https://image.tmdb.org/t/p/w400/'+ movie.poster_path" style="width:18rem; height:25rem;" class="card-img-top" alt="...">
         </div>
       </router-link>
     </div>

405af144200df1b177a2067b0fd6ea237341ccf0
holawan <asdf134652@gmail.com>
2022-05-25 10:26:56 +0900

    [fix] movie list

---
+++ final-pjt-front/src/components/MovieList2.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <movie-list-item
+      v-for="movie in movies"
+      :key="movie.id"
+      :movie="movie"
+    ></movie-list-item>
+        <vue-glide v-if="movies.length"
+      class="glide__track m-4"
+      data-glide-el="track"
+      ref="slider"
+      type="carousel"
+      :breakpoints="{3000: {perView: 5}, 1100: {perView: 4}, 600: {perView: 3}}"
+    >
+      <vue-glide-slide
+        v-for = "(movie, idx) in movies"
+        :key="idx">
+        
+        <movie-list-carousel
+          :movie="movie"
+        />
+        
+      </vue-glide-slide>
+    </vue-glide>
+  </div>
+</template>
+
+<script>
+import MovieListCarousel from './MovieListCarousel.vue'
+// import MovieListItem from './MovieListItem.vue'
+
+export default {
+  name:'MovieList',
+  components:{MovieListItem, MovieListCarousel},
+  props:{
+    movies:Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- final-pjt-front/src/main.js
+++ final-pjt-front/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
+import VueGlide from 'vue-glide-js'
+import 'vue-glide-js/dist/vue-glide.css'
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,6 +12,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueGlide)
 Vue.config.productionTip = false
 //src/main.js
 // import Vue from 'vue'
